3fe2fe465493b9207a6d05b2bbda1c4b0f39efd7
FoxWeb <dneprfox@gmail.com>
2019-11-12 18:50:21 +0200

    added page & modal

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function(){ 
   
 
   $(".rate-star").rateYo({
@@ -59,6 +59,14 @@ $(function(){
     $('.icon-th-list').removeClass('active');
   });
 
+  $('.menu__btn').on("click", function(){
+    $(this).toggleClass('open');
+    $('.menu__list').slideToggle();
+  });
+  
+  $('.header__btn-menu').on('click', function () {
+    $('.header__box').toggleClass('active');
+  });
 
   var mixer = mixitup('.products__inner-box');
 
